5b195ace938855bdc47a221090b5c8128219ffdd
Ernest Wamae <ernestriunge13@outlook.com>
2024-08-15 16:36:14 +0300

    first commit

---
+++ assignment.py
@@ -0,0 +1,67 @@
+# Part 1
+
+# Question 1
+
+# Introduction
+fullName = "Ernest Wamae"
+myAge = 15
+favColour = "Orange"
+cityLive = "Nairobi"
+
+# Question 2
+
+# Printing introduction
+print(f"My name is {fullName}")
+print(f"I am {myAge} years old")
+print(f"My favourite colour is {favColour}")
+print(f"I live in {cityLive}")
+
+# Part 2
+
+# Question 1
+
+# Assigning the two numbers
+x = 5
+y = 10
+# Basic Operations
+z = x + y
+a = y - x
+b = x * y
+c = y / x
+
+print(z)
+print(a)
+print(b)
+print(c)
+
+# Question 2
+currentAge = 15
+futureAge = currentAge + 5
+
+print(f"I am going to be {futureAge} in five years")
+
+# Advance Operations
+pi = 3.142
+# This can vary
+radius = 14
+# Area of a circle
+area = pi * radius * radius
+
+print(f"The area of the circle is {area}cm²")
+
+# Part 3 (Complex Question)
+
+# Question 1
+
+# Quoting
+favQuote = "Be yourself, everyody else is taken"
+
+print(favQuote, " ~ Ernest Wamae");
+
+#Question 2
+friendName = "Brian"
+greetingOne = "Wsg Bro"
+greetingTwo = "Yea am ayt"
+
+print(f"Brian : {greetingOne}")
+print(f"Ernest : {greetingTwo}")
